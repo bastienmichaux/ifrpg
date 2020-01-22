41e818fc35f4a4faae8cbb56d67263e830ba66a1
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-01-22 23:23:40 +0100

    temp start screen button methods

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,34 @@
+const niy = () => console.warn('niy');
+
 class StartView {
-  static render() {
-    const niy = () => console.warn('niy');
-    const app = document.getElementById('app');
-    const frg = new DocumentFragment();
+  static newGame(): void {
+    niy();
+  }
+  static continueGame(): void {
+    niy();
+  }
+  static render(): void {
+    const savedGameExists = false; // #%
+    // new game button
     const newGameBtn = document.createElement('button');
     newGameBtn.textContent = 'new game';
-    newGameBtn.addEventListener('mouseup', niy);
+    newGameBtn.addEventListener('mouseup', StartView.newGame);
     newGameBtn.style.display = 'block';
+    // continue game button
     const continueGameBtn = document.createElement('button');
     continueGameBtn.textContent = 'continue game';
-    continueGameBtn.addEventListener('mouseup', niy);
+    continueGameBtn.addEventListener('mouseup', StartView.continueGame);
+    continueGameBtn.disabled = savedGameExists;
     newGameBtn.style.display = 'block';
+    // start screen title
     const title = document.createElement('p');
     title.textContent = 'ifprg';
+    // assembly
+    const frg = new DocumentFragment();
     frg.appendChild(title);
     frg.appendChild(newGameBtn);
     frg.appendChild(continueGameBtn);
-    app.appendChild(frg);
+    document.getElementById('app').appendChild(frg);
   }
 }
 
