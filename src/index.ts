0e38f4130933573f1bf395ec65b5381c8d83164a
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-01-22 23:16:22 +0100

    impl StartScreen class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
-const niy = () => console.warn('niy');
-const app = document.getElementById('app');
-const frg = new DocumentFragment();
-const newGameBtn = document.createElement('button');
-newGameBtn.textContent = 'new game';
-newGameBtn.addEventListener('mouseup', niy);
-const continueGameBtn = document.createElement('button');
-continueGameBtn.textContent = 'continue game';
-continueGameBtn.addEventListener('mouseup', niy);
-const title = document.createElement('p');
-title.textContent = 'ifprg'
-frg.appendChild(title);
-frg.appendChild(newGameBtn);
-frg.appendChild(continueGameBtn);
-app.appendChild(frg);
+class StartView {
+  static render() {
+    const niy = () => console.warn('niy');
+    const app = document.getElementById('app');
+    const frg = new DocumentFragment();
+    const newGameBtn = document.createElement('button');
+    newGameBtn.textContent = 'new game';
+    newGameBtn.addEventListener('mouseup', niy);
+    newGameBtn.style.display = 'block';
+    const continueGameBtn = document.createElement('button');
+    continueGameBtn.textContent = 'continue game';
+    continueGameBtn.addEventListener('mouseup', niy);
+    newGameBtn.style.display = 'block';
+    const title = document.createElement('p');
+    title.textContent = 'ifprg';
+    frg.appendChild(title);
+    frg.appendChild(newGameBtn);
+    frg.appendChild(continueGameBtn);
+    app.appendChild(frg);
+  }
+}
+
+StartView.render();
