f204ddbb9e31138345d400259e1f2d8986d37fcc
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-01-22 23:09:57 +0100

    ugly

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+const niy = () => console.warn('niy');
 const app = document.getElementById('app');
-app.textContent = 'hello';
+const frg = new DocumentFragment();
+const newGameBtn = document.createElement('button');
+newGameBtn.textContent = 'new game';
+newGameBtn.addEventListener('mouseup', niy);
+const continueGameBtn = document.createElement('button');
+continueGameBtn.textContent = 'continue game';
+continueGameBtn.addEventListener('mouseup', niy);
+const title = document.createElement('p');
+title.textContent = 'ifprg'
+frg.appendChild(title);
+frg.appendChild(newGameBtn);
+frg.appendChild(continueGameBtn);
+app.appendChild(frg);
