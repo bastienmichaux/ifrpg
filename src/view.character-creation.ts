f1e569b6662537d90604fd3dbb88d4d324062d91
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-01-26 00:55:52 +0100

    character creation view: add classes, small improvements

--- src/view.character-creation.ts
+++ src/view.character-creation.ts
@@ -16,24 +16,34 @@ export class CharacterCreationView {
     });
 
     // name input
-    const nameInput = DomService.getTextInputDiv({
+    const nameInput = DomService.getTextInput({
       id: 'name',
       name: 'Name'
     });
 
+    // class input
+    const classInput = DomService.getRadioInputs({
+      id: 'class',
+      values: ['Warrior', 'Thief', 'Wizard']
+    });
+
     // submit button
     const submitBtn = DomService.getButton({
-      text: 'create character',
+      text: 'Create character',
       click: this.submitCharacter
     });
-    submitBtn.style.display = 'block';
+    submitBtn.className = 'blocky';
 
     // assembly
     AppService.clear();
     const frg = new DocumentFragment();
     frg.appendChild(title);
+    frg.appendChild(DomService.getP('Character gender'));
     frg.appendChild(genderInput);
+    frg.appendChild(DomService.getP('Character name'));
     frg.appendChild(nameInput);
+    frg.appendChild(DomService.getP('Character class'));
+    frg.appendChild(classInput);
     frg.appendChild(submitBtn);
     document.getElementById('app').appendChild(frg);
   }
