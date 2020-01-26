26dedbfdd6eb60505f32872b5073419897fb2ef0
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-01-26 15:55:19 +0100

    move buttons for switching views to a component

---
+++ src/component.switch-view.ts
@@ -0,0 +1,46 @@
+import { DomService } from "./service.dom";
+
+export class SwitchViewComponent {
+  static render(): DocumentFragment {
+    // buttons
+    const inventoryBtn = DomService.getButton({
+      text: 'Inventory',
+      click: () => console.log('niy')
+    });
+    const characterBtn = DomService.getButton({
+      text: 'Character',
+      click: () => console.log('niy')
+    });
+    const diaryBtn = DomService.getButton({
+      text: 'Diary',
+      click: () => console.log('niy')
+    });
+    const travelBtn = DomService.getButton({
+      text: 'Travel',
+      click: () => console.log('niy')
+    });
+    const explorationBtn = DomService.getButton({
+      text: 'Exploration',
+      click: () => console.log('niy')
+    });
+    const spellbookBtn = DomService.getButton({
+      text: 'Spellbook',
+      click: () => console.log('niy')
+    });
+    const settingsBtn = DomService.getButton({
+      text: 'Menu',
+      click: () => console.log('niy')
+    });
+
+    // assembly
+    const frg = new DocumentFragment;
+    frg.appendChild(explorationBtn);
+    frg.appendChild(inventoryBtn);
+    frg.appendChild(characterBtn);
+    frg.appendChild(diaryBtn);
+    frg.appendChild(spellbookBtn);
+    frg.appendChild(travelBtn);
+    frg.appendChild(settingsBtn);
+    return frg;
+  }
+}
--- src/view.exploration.ts
+++ src/view.exploration.ts
@@ -1,52 +1,17 @@
 import { DomService } from "./service.dom";
 import { AppService } from "./service.app";
+import { SwitchViewComponent } from "./component.switch-view";
 
 export class ExplorationView {
   static render() {
     // time - day
     const dayP = DomService.getP('NoName character - Day 1 - noon');
 
-    // aside
-    const inventoryBtn = DomService.getButton({
-      text: 'Inventory',
-      click: () => console.log('niy')
-    });
-    const characterBtn = DomService.getButton({
-      text: 'Character',
-      click: () => console.log('niy')
-    });
-    const diaryBtn = DomService.getButton({
-      text: 'Diary',
-      click: () => console.log('niy')
-    });
-    const travelBtn = DomService.getButton({
-      text: 'Travel',
-      click: () => console.log('niy')
-    });
-    const explorationBtn = DomService.getButton({
-      text: 'Exploration',
-      click: () => console.log('niy')
-    });
-    const spellbookBtn = DomService.getButton({
-      text: 'Spellbook',
-      click: () => console.log('niy')
-    });
-    const settingsBtn = DomService.getButton({
-      text: 'Menu',
-      click: () => console.log('niy')
-    });
-
     // assembly
     AppService.clear();
     const frg = new DocumentFragment();
     frg.appendChild(dayP);
-    frg.appendChild(explorationBtn);
-    frg.appendChild(inventoryBtn);
-    frg.appendChild(characterBtn);
-    frg.appendChild(diaryBtn);
-    frg.appendChild(spellbookBtn);
-    frg.appendChild(travelBtn);
-    frg.appendChild(settingsBtn);
+    frg.appendChild(SwitchViewComponent.render());
     document.getElementById('app').appendChild(frg);
   }
 }
