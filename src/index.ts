36ef52d0a6097b31385c7d839ddcfa3e41610d0d
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-01-26 16:03:05 +0100

    add placeholder location + time

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { CharacterCreationView } from './view.character-creation';
 import { StartView } from './view.start';
+import { ExplorationView } from './view.exploration';
 
 // StartView.render();
-CharacterCreationView.render();
+// CharacterCreationView.render();
+ExplorationView.render();
--- src/view.exploration.ts
+++ src/view.exploration.ts
@@ -4,13 +4,18 @@ import { SwitchViewComponent } from "./component.switch-view";
 
 export class ExplorationView {
   static render() {
+    // stored data
+    const location = 'No location'; // #%
+    const time = 'Day 1 - noon'; // #%
+
     // time - day
-    const dayP = DomService.getP('NoName character - Day 1 - noon');
+    // TODO: left align location, right align time
+    const statusP = DomService.getP(`${location} - ${time}`);
 
     // assembly
     AppService.clear();
     const frg = new DocumentFragment();
-    frg.appendChild(dayP);
+    frg.appendChild(statusP);
     frg.appendChild(SwitchViewComponent.render());
     document.getElementById('app').appendChild(frg);
   }
