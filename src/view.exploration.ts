c4a275f0b93dff3108eb599e40e72b30db1ed728
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-01-26 15:39:34 +0100

    add buttons for main views

--- src/view.exploration.ts
+++ src/view.exploration.ts
@@ -3,10 +3,50 @@ import { AppService } from "./service.app";
 
 export class ExplorationView {
   static render() {
+    // time - day
+    const dayP = DomService.getP('NoName character - Day 1 - noon');
+
+    // aside
+    const inventoryBtn = DomService.getButton({
+      text: 'Inventory',
+      click: () => console.log('niy')
+    });
+    const characterBtn = DomService.getButton({
+      text: 'Character',
+      click: () => console.log('niy')
+    });
+    const diaryBtn = DomService.getButton({
+      text: 'Diary',
+      click: () => console.log('niy')
+    });
+    const travelBtn = DomService.getButton({
+      text: 'Travel',
+      click: () => console.log('niy')
+    });
+    const explorationBtn = DomService.getButton({
+      text: 'Exploration',
+      click: () => console.log('niy')
+    });
+    const spellbookBtn = DomService.getButton({
+      text: 'Spellbook',
+      click: () => console.log('niy')
+    });
+    const settingsBtn = DomService.getButton({
+      text: 'Menu',
+      click: () => console.log('niy')
+    });
+
     // assembly
     AppService.clear();
     const frg = new DocumentFragment();
-    frg.appendChild(DomService.getP('Exploration view'));
+    frg.appendChild(dayP);
+    frg.appendChild(explorationBtn);
+    frg.appendChild(inventoryBtn);
+    frg.appendChild(characterBtn);
+    frg.appendChild(diaryBtn);
+    frg.appendChild(spellbookBtn);
+    frg.appendChild(travelBtn);
+    frg.appendChild(settingsBtn);
     document.getElementById('app').appendChild(frg);
   }
 }
