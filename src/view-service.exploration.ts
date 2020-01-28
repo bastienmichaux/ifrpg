ef826b85d8b5bd42248dfe8ec0fe1bb419255e25
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-01-29 00:34:58 +0100

    daytime to string

---
+++ src/view-service.exploration.ts
@@ -0,0 +1,17 @@
+export class ExplorationViewService {
+  static dayTimeToString(daytime: number): string {
+    // TODO: check daytime < 24*60*60
+    // TODO: daytime f day of the year
+    // TODO: unknown/assumed daytime if inside since > (n hours inside + (avg(intelligence+perception)/2) - 20)
+    let daytimeStr = '';
+    if (daytime >= 0 && daytime < 4 * 60 * 60) daytimeStr = 'late night';
+    if (daytime >= 4 * 60 * 60 && daytime < 6 * 60 * 60) daytimeStr = 'early morning';
+    if (daytime >= 6 * 60 * 60 && daytime < 11 * 60 * 60) daytimeStr = 'morning';
+    if (daytime >= 11 * 60 * 60 && daytime < 13 * 60 * 60) daytimeStr = 'noon';
+    if (daytime >= 13 * 60 * 60 && daytime < 14 * 60 * 60) daytimeStr = 'early afternoon';
+    if (daytime >= 14 * 60 * 60 && daytime < 18 * 60 * 60) daytimeStr = 'afternoon';
+    if (daytime >= 18 * 60 * 60 && daytime < 20 * 60 * 60) daytimeStr = 'evening';
+    if (daytime >= 20 * 60 * 60 && daytime <= 24 * 60 * 60) daytimeStr = 'night';
+    return daytimeStr;
+  }
+}
