db37b8fd076c54d23870610cbffa6b43e97c2a86
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-02-03 00:42:10 +0100

    remove unused method

--- src/service.character.ts
+++ src/service.character.ts
@@ -1,11 +1,5 @@
-import { Character } from "./model.character";
-
 export class CharacterService {
-  static getDescription(ch: Character): string {
-    return `${ch.name} is a ${CharacterService.genderToString(ch.gender)}`;
-  }
-
   static genderToString(gender: boolean): string {
-    return gender === true ? 'man' : 'woman';
+    return gender ? 'man' : 'woman';
   }
 }
