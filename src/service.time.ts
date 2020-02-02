5d50ed2c897197db5ea836eec50ddfab5e9065ff
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-02-03 00:41:31 +0100

    improve time to string

--- src/service.time.ts
+++ src/service.time.ts
@@ -5,17 +5,16 @@ export class TimeService {
     // TODO: realistic daytime
     // TODO: daytime f day of the year
     // TODO: unknown/assumed daytime if inside since > (n hours inside + (avg(intelligence+perception)/2) - 20)
-    let daytimeStr = '';
-    if (daytime >= 0 && daytime < 4 * 60 * 60) daytimeStr = 'late night';
-    if (daytime >= 4 * 60 * 60 && daytime < 6 * 60 * 60) daytimeStr = 'early morning';
-    if (daytime >= 6 * 60 * 60 && daytime < 7 * 60 * 60) daytimeStr = 'dawn';
-    if (daytime >= 7 * 60 * 60 && daytime < 11 * 60 * 60) daytimeStr = 'morning';
-    if (daytime >= 11 * 60 * 60 && daytime < 13 * 60 * 60) daytimeStr = 'noon';
-    if (daytime >= 13 * 60 * 60 && daytime < 14 * 60 * 60) daytimeStr = 'early afternoon';
-    if (daytime >= 14 * 60 * 60 && daytime < 15 * 60 * 60) daytimeStr = 'afternoon';
-    if (daytime >= 15 * 60 * 60 && daytime < 18 * 60 * 60) daytimeStr = 'twilight';
-    if (daytime >= 18 * 60 * 60 && daytime < 20 * 60 * 60) daytimeStr = 'evening';
-    if (daytime >= 20 * 60 * 60 && daytime <= 24 * 60 * 60) daytimeStr = 'night';
-    return daytimeStr;
+    if (daytime >= 0 && daytime < 4 * 60 * 60) return 'late night';
+    if (daytime >= 4 * 60 * 60 && daytime < 6 * 60 * 60) return 'early morning';
+    if (daytime >= 6 * 60 * 60 && daytime < 7 * 60 * 60) return 'dawn';
+    if (daytime >= 7 * 60 * 60 && daytime < 11 * 60 * 60) return 'morning';
+    if (daytime >= 11 * 60 * 60 && daytime < 13 * 60 * 60) return 'noon';
+    if (daytime >= 13 * 60 * 60 && daytime < 14 * 60 * 60) return 'early afternoon';
+    if (daytime >= 14 * 60 * 60 && daytime < 15 * 60 * 60) return 'afternoon';
+    if (daytime >= 15 * 60 * 60 && daytime < 18 * 60 * 60) return 'twilight';
+    if (daytime >= 18 * 60 * 60 && daytime < 20 * 60 * 60) return 'evening';
+    if (daytime >= 20 * 60 * 60 && daytime <= 24 * 60 * 60) return 'night';
+    else throw new Error(`unknown daytime ${daytime}`)
   }
 }
