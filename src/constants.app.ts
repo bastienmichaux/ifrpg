0b3a753cc8393db792b11f6bb24c70957a7f59d1
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-02-09 21:21:35 +0100

    update styles and ids

--- src/constants.app.ts
+++ src/constants.app.ts
@@ -2,3 +2,16 @@ export const CONSTANTS = {
   BASE_HP: 10,
   BASE_PRIMARY_CHARACTERISTIC_SCORE: 10,
 };
+
+export const enum ID {
+  APP = 'app',
+  CHARACTER_CREATION_VIEW = 'character-creation-view',
+  CHARACTER_VIEW = 'character-view',
+  DIARY_VIEW = 'diary-view',
+  EXPLORATION_VIEW = 'exploration-view',
+  INVENTORY_VIEW = 'inventory-view',
+  SETTINGS_VIEW = 'settings-view',
+  SPELLBOOK_VIEW = 'spellbook-view',
+  START_VIEW = 'start-view',
+  TRAVEL_VIEW = 'travel-view',
+};
