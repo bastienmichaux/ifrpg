4c266997696607587e166fe8fe5037c9f95d8075
bastienmichaux@gmail.com <bastienmichaux@gmail.com>
2020-02-09 13:37:55 +0100

    hardcoded breadcrumbs

--- src/component.status.ts
+++ src/component.status.ts
@@ -3,15 +3,21 @@ import { TimeService } from "./service.time";
 import { StoreService } from "./service.store";
 
 export class StatusComponent {
-  static getComponent(): HTMLElement {
+  static getComponent(): DocumentFragment {
     // stored data
     // TODO: game start ? random gen : get it from storage
     const location = 'No location';
     const time = StoreService.loadDate();
     const timeStr = `Day 1 - ${TimeService.getTimeToString(time)}`;
 
+    // TODO: breadcrumbs from store
+    const travelCrumbs = DomService.getP(`Region > Settlement > Room`);
+
     // time - day
     // TODO: left align location, right align time
-    return DomService.getP(`${location} - ${timeStr}`);
+    const frg = new DocumentFragment();
+    frg.appendChild(travelCrumbs);
+    frg.appendChild(DomService.getP(`${location} - ${timeStr}`));
+    return frg;
   }
 }
